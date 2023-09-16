2d74b0d30623b48da79342b68feba30b9e750fc8
Lívia Nascimento <liviapnd@gmail.com>
2023-09-16 15:33:23 -0300

    feat: dump do banco de dados

---
+++ dump.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE dindin;
+
+CREATE TABLE usuarios (
+	id SERIAL PRIMARY KEY, 
+	nome VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	senha TEXT NOT NULL
+);
+
+CREATE TABLE categorias (
+	id SERIAL PRIMARY KEY,
+	descricao VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE transacoes (
+	id SERIAL PRIMARY KEY,
+	descricao VARCHAR(255) NOT NULL,
+	valor INT NOT NULL,
+	data DATE DEFAULT NOW(),
+	categoria_id INT REFERENCES categorias(id) NOT NULL,
+	usuario_id INT REFERENCES usuarios(id) NOT NULL,
+	tipo VARCHAR(25) NOT NULL
+);
