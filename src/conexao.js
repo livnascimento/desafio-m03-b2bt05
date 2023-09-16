f7bc480abf35e121c8d9f9db5bbdcadd3a832104
Lívia Nascimento <liviapnd@gmail.com>
2023-09-16 16:19:36 -0300

    fix: conexão do bd com variáveis de ambiente

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
+require('dotenv').config()
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const database = process.env.DATABASE;
 
 const pool = new Pool({
-    host: '',
-    port: ,
-    user: '',
-    password: '',
-    database: ''
+    host: host,
+    port: port,
+    user: user,
+    password: password,
+    database: database
 });
 
 module.exports = pool;
