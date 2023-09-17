ae3bbf054bc422736fe190f40afe0eacbd74191d
Marriett Albuquerque <marriettwt@gmail.com>
2023-09-17 15:37:56 -0300

    feat: login de usuário

--- src/controladores/usuarioControlador.js
+++ src/controladores/usuarioControlador.js
@@ -1,5 +1,7 @@
 const pool = require('../conexao');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv/config');
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body;
@@ -19,6 +21,35 @@ const cadastrarUsuario = async (req, res) => {
 
 };
 
+const login = async (req, res) => {
+    const { email, senha } = req.body;
+    const senhaJwt = process.env.SENHA_JWT;
+
+    if (!email || !senha) return res.status(400).json({mensagem:"Todos os campos são obrigatórios."});
+
+    try {
+        const usuario = await pool.query(`select * from usuarios where email = $1`, [email]);
+
+        if (usuario.rowCount === 0) return res.status(400).json({mensagem:"Credenciais inválidas."});
+
+        const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha);
+
+        if (!senhaValida) return res.status(400).json({mensagem:"Credenciais inválidas."});
+
+        const token = jwt.sign({id: usuario.rows[0].id}, senhaJwt, {expiresIn: '8h'});
+
+        const { senha: _, ...usuarioLogado } = usuario.rows[0];
+
+        return res.status(201).json({usuarioLogado, token});
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ mensagem: `erro interno do servidor` });
+    }
+
+};
+
 module.exports = {
-    cadastrarUsuario
+    cadastrarUsuario,
+    login
 };
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const rotas = express();
-const { cadastrarUsuario } = require('./controladores/usuarioControlador');
+const { 
+    cadastrarUsuario, 
+    login 
+} = require('./controladores/usuarioControlador');
 const { validarCadastro } = require('./intermediarios/usuarioIntermediario');
 
 rotas.post('/usuario', validarCadastro, cadastrarUsuario);
+rotas.post('/login', login);
 
 module.exports = rotas;
