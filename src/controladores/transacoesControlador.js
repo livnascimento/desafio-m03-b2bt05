6e7ed6eea8db290247c5a82f9e9176a90ed070f8
sahrarafaela <sararafaela704@gmail.com>
2023-09-18 17:38:54 -0300

    fix: async/await

--- src/controladores/transacoesControlador.js
+++ src/controladores/transacoesControlador.js
@@ -1,13 +1,13 @@
 const pool = require("../conexao");
 
-const listarTransacoes = (req, res) => {
+const listarTransacoes = async (req, res) => {
 
     const { id } = req.usuario;
 
     try {
         const query = "select * from  transacoes where usuario_id = $1";
         const params = [id];
-        const transacoes = pool.query(query, params);
+        const transacoes = await pool.query(query, params);
 
         return res.json(transacoes.rows)
     } catch (error) {
