ee1f175c9fb5fb0a41a3b92c76e4cc6d73c832c2
sahrarafaela <sararafaela704@gmail.com>
2023-09-18 17:14:41 -0300

    fix: adicionando resposta e listando  categorias

--- src/controladores/categoriaControlador.js
+++ src/controladores/categoriaControlador.js
@@ -2,6 +2,13 @@ const pool = require("../conexao");
 
 const listarCategorias = async (req, res) => {
     const query = `select * from categorias`;
-
-    const categorias = await pool.query(query);
+    try {
+        const categorias = await pool.query(query);
+        return res.json(categorias);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ mensagem: "Erro interno do servidor." });
+    }
 };
+
+module.exports = { listarCategorias };
